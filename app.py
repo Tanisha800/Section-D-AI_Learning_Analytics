c75d92b6fc0b22225b2f2e0f42471b7f8b292668
adamyatiwari12 <adamyatiwari99@gmail.com>
2026-02-15 01:19:45 +0530

    feat: add logistic regression model

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.linear_model import LogisticRegression,LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,precision_score, root_mean_squared_error
+
 def load_data(path):
     df = pd.read_csv(path)
     return df
@@ -28,6 +33,7 @@ def preprocess_data(df):
     # Drop rows without Result column
     df = df.dropna(subset=['Result'])
 
+    # Add new features
     df["avg_quiz"] = df[["Quiz 1", "Quiz 2", "Quiz 3"]].mean(axis=1)
 
     df["total_score"] = df[
@@ -35,3 +41,33 @@ def preprocess_data(df):
     ].mean(axis=1)
 
     return df
+
+def logistic_regression(df):
+
+    features = [
+        "avg_quiz",
+        "Assignment Score",
+        "Midterm",
+        "Time Spent (hrs/week)"
+    ]
+
+    X = df[features]
+    y = df["Result"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
+
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    model = LogisticRegression()
+    model.fit(X_train_scaled, y_train)
+
+    y_pred = model.predict(X_test_scaled)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+
+    return accuracy, precision
+
+
