8f58e4ca6e112ae546cdeb95a275ebeefe6d9443
adamyatiwari12 <adamyatiwari99@gmail.com>
2026-04-14 15:13:58 +0530

    feat: integrate Groq LLM for generating study coach responses

---
+++ agent/llm.py
@@ -0,0 +1,20 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def generate_response(prompt):
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role": "system", "content": "You are an AI Study Coach."},
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0.7,
+        max_tokens=300
+    )
+
+    return response.choices[0].message.content
